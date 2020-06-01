e9f8840ea4285f49c3db95aa52f06c640a4e6277
rasumma <rasumma@me.com>
2020-06-01 14:59:09 -0500

    busPatrolChallenge.py

--- busPatrolChallenge.py
+++ busPatrolChallenge.py
@@ -1,5 +1,3 @@
-
-
 from flask import Flask, jsonify, request, json
 import sqlite3 as sqlite
 import sys
@@ -7,17 +5,14 @@ import sys
 
 app = Flask(__name__)
 
-#attempt connection to busPatrol database
+#if no name is specified in paramaters, list all valid names for the user to choose from by creating a cursor
+#to fetch all of the the data from the users table and appending them all to a string to be jsonify'd
 
-
-
-#if no name is specified in paramaters, list all names for the user to choose.
 @app.route('/users', methods=['GET'])
 def listNames():
 	jsonText = ""
 	connection = sqlite.connect('buspatrol.db')
 	with connection:	
-		
 		cursor = connection.cursor()
 		cursor.execute("SELECT * FROM users")
 		rows = cursor.fetchall()
@@ -26,21 +21,26 @@ def listNames():
 			jsonText += row[1] + "  " 
 
 	return jsonify(users=jsonText) 
+
+	
+#if a name is specified, then we use whatever name they give us and check the users table for the given name.
+#if it is a valid name then it will set the jobID variable to whatever jobID that user has in the table,
+#otherwise the userRows object will be set as None, so I handle that error by returning a fairly generic error statement
+#with information on how to search for valid names. We then use the jobID to search through the jobs table and we get the title
+#and description from the table and jsonify them.
 		
-
-
 @app.route('/users/<string:name>', methods=['GET'])
 def listUserDescription(name):
 	jsonText = ""
 	jobID = 0
-
 	jobsConnection = sqlite.connect('buspatrol.db')
 	usersConnection = sqlite.connect('buspatrol.db')
-	with usersConnection:
 
+	with usersConnection:
 		usersCursor = usersConnection.cursor()
 		usersCursor.execute("SELECT name, job  FROM users WHERE name=:name", {"name" : name})
 		usersRows = usersCursor.fetchone()
+
 		if usersRows is None:
 			jsonText = "Not a valid user. Please check /users for valid users."
 			return jsonify(jsonText)
@@ -48,12 +48,9 @@ def listUserDescription(name):
 			jobID = usersRows[1]
 
 	with jobsConnection:
-
 		jobsCursor = jobsConnection.cursor()
 		jobsCursor.execute("SELECT id, title, description FROM jobs WHERE id=:id", {"id" : jobID})
 		jobsRows = jobsCursor.fetchone()
 
-	
 	return jsonify(job_title=jobsRows[1],
 			job_description=jobsRows[2])
-		
